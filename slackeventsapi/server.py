6d8ff4f125603117e3f8c561cb69f0558640d9df
roach <urbandev@gmail.com>
2018-01-16 11:48:48 -0800

    Add the ability for SlackServer to accept existing Flask instance as a param
    Update test dependencies to latest versions, update pyee to 5.0.5
    
    Added newline to appease flake8
    
    Add python 3 to test environments

--- slackeventsapi/server.py
+++ slackeventsapi/server.py
@@ -3,11 +3,24 @@ import json
 
 
 class SlackServer(Flask):
-    def __init__(self, verification_token, endpoint, emitter):
-        Flask.__init__(self, __name__)
+    def __init__(self, verification_token, endpoint, emitter, server):
         self.verification_token = verification_token
+        self.emitter = emitter
+        self.endpoint = endpoint
 
-        @self.route(endpoint, methods=['GET', 'POST'])
+        # If a server is passed in, bind the event handler routes to it,
+        # otherwise create a new Flask instance.
+        if server:
+            if isinstance(server, Flask):
+                self.bind_route(server)
+            else:
+                raise TypeError("Server must be an instance of Flask")
+        else:
+            Flask.__init__(self, __name__)
+            self.bind_route(self)
+
+    def bind_route(self, server):
+        @server.route(self.endpoint, methods=['GET', 'POST'])
         def event():
             # If a GET request is made, return 404.
             if request.method == 'GET':
@@ -25,11 +38,11 @@ class SlackServer(Flask):
             # Verify the request token
             request_token = event_data.get("token")
             if self.verification_token != request_token:
-                emitter.emit('error', 'invalid verification token')
+                self.emitter.emit('error', 'invalid verification token')
                 return make_response("Request contains invalid Slack verification token", 403)
 
             # Parse the Event payload and emit the event to the event listener
             if "event" in event_data:
                 event_type = event_data["event"]["type"]
-                emitter.emit(event_type, event_data)
+                self.emitter.emit(event_type, event_data)
                 return make_response("", 200)
