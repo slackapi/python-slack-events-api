5bafd9c372b700eb89138b65e4f84202ea8f84fa
roach <urbandev@gmail.com>
2018-08-14 14:57:08 -0700

    Added more tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,6 +2,7 @@ import json
 from flask import Flask
 import pytest
 import sys
+import hmac
 import time
 from slackeventsapi import SlackEventAdapter
 from slackeventsapi.server import SlackEventAdapterException
@@ -68,6 +69,50 @@ def test_invalid_request_signature(client):
     assert str(excinfo.value) == 'Invalid request signature'
 
 
+def test_invalid_request_timestamp(client):
+    # Verify [package metadata header is set
+    slack_adapter = SlackEventAdapter("SIGNING_SECRET")
+
+    data = pytest.reaction_event_fixture
+    timestamp = int(time.time()+1000)
+    signature = "bad timestamp"
+
+    with pytest.raises(SlackEventAdapterException) as excinfo:
+        res = client.post(
+            '/slack/events',
+            data=data,
+            content_type='application/json',
+            headers={
+                'X-Slack-Request-Timestamp': timestamp,
+                'X-Slack-Signature': signature
+            }
+        )
+
+    assert str(excinfo.value) == 'Invalid request timestamp'
+
+
+def test_compare_digest_fallback(client, monkeypatch):
+    # Verify [package metadata header is set
+    slack_adapter = SlackEventAdapter("SIGNING_SECRET")
+    monkeypatch.delattr(hmac, 'compare_digest')
+
+    data = pytest.reaction_event_fixture
+    timestamp = int(time.time())
+    signature =pytest.create_signature(slack_adapter.signing_secret, timestamp, data)
+
+    res = client.post(
+        '/slack/events',
+        data=data,
+        content_type='application/json',
+        headers={
+            'X-Slack-Request-Timestamp': timestamp,
+            'X-Slack-Signature': signature
+        }
+    )
+
+    assert res.status_code == 200
+
+
 def test_version_header(client):
     # Verify [package metadata header is set
     slack_adapter = SlackEventAdapter("SIGNING_SECRET")
@@ -97,3 +142,10 @@ def test_server_start(mocker):
     mocker.spy(slack_events_adapter, 'server')
     slack_events_adapter.start(port=3000)
     slack_events_adapter.server.run.assert_called_once_with(debug=False, host='127.0.0.1', port=3000)
+
+
+def test_default_exception_msg(mocker):
+    with pytest.raises(SlackEventAdapterException) as excinfo:
+        raise SlackEventAdapterException
+
+    assert str(excinfo.value) == 'An error occurred in the SlackEventsApiAdapter library'
