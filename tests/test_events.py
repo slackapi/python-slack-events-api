16961ab2a46284b1cbec3bbd92f4249db2d41b1e
datashaman <marlinf@datashaman.com>
2018-10-14 11:19:43 +0200

    Ensure event handler is called in event test

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,27 +1,31 @@
 import time
 import pytest
-from slackeventsapi import SlackEventAdapter
 
-ADAPTER = SlackEventAdapter('SIGNING_SECRET')
+def test_event_emission(adapter):
+    test_event_emission.event_handled = False
 
-def test_event_emission(client):
     # Events should trigger an event
-    @ADAPTER.on('reaction_added')
-    def event_handler(event):
+    @adapter.on('reaction_added')
+    def event_handler(event_data):
+        test_event_emission.event_handled = True
+
+        event = event_data['event']
         assert event["reaction"] == 'grinning'
 
     data = pytest.reaction_event_fixture
     timestamp = int(time.time())
-    signature = pytest.create_signature(ADAPTER.signing_secret, timestamp, data)
+    signature = pytest.create_signature(adapter.signing_secret, timestamp, data)
 
-    res = client.post(
-        '/slack/events',
-        data=data,
-        content_type='application/json',
-        headers={
-            'X-Slack-Request-Timestamp': timestamp,
-            'X-Slack-Signature': signature
-        }
-    )
+    with adapter.server.test_client() as client:
+        res = client.post(
+            '/slack/events',
+            data=data,
+            content_type='application/json',
+            headers={
+                'X-Slack-Request-Timestamp': timestamp,
+                'X-Slack-Signature': signature
+            }
+        )
+        assert res.status_code == 200
 
-    assert res.status_code == 200
+    assert test_event_emission.event_handled
