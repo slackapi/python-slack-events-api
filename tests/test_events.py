804e3b24a3b9ad18d33ba211c1d9752ede4ec5ed
datashaman <marlinf@datashaman.com>
2018-10-14 12:47:07 +0200

    Add tests for error emitters

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,6 +1,8 @@
 import time
 import pytest
 
+from slackeventsapi.server import SlackEventAdapterException
+
 def test_event_emission(adapter):
     test_event_emission.event_handled = False
 
@@ -29,3 +31,62 @@ def test_event_emission(adapter):
         assert res.status_code == 200
 
     assert test_event_emission.event_handled
+
+def test_error_timestamp(adapter):
+    test_error_timestamp.event_handled = False
+
+    # Error should trigger an event
+    @adapter.on('error')
+    def event_handler(exception):
+        test_error_timestamp.event_handled = True
+        assert isinstance(exception, SlackEventAdapterException)
+        assert str(exception) == 'Invalid request timestamp'
+
+    data = pytest.reaction_event_fixture
+
+    # Set timestamp to Thu Jan 01 00:00:00 1970 UTC (Epoch 0)
+    timestamp = 0
+
+    signature = pytest.create_signature(adapter.signing_secret, timestamp, data)
+
+    with adapter.server.test_client() as client:
+        res = client.post(
+            '/slack/events',
+            data=data,
+            content_type='application/json',
+            headers={
+                'X-Slack-Request-Timestamp': timestamp,
+                'X-Slack-Signature': signature
+            }
+        )
+        assert res.status_code == 403
+
+    assert test_error_timestamp.event_handled
+
+def test_error_signature(adapter):
+    test_error_signature.event_handled = False
+
+    # Error should trigger an event
+    @adapter.on('error')
+    def event_handler(exception):
+        test_error_signature.event_handled = True
+        assert isinstance(exception, SlackEventAdapterException)
+        assert str(exception) == 'Invalid request signature'
+
+    data = pytest.reaction_event_fixture
+    timestamp = int(time.time())
+    signature = pytest.create_signature('INVALID SIGNATURE', timestamp, data)
+
+    with adapter.server.test_client() as client:
+        res = client.post(
+            '/slack/events',
+            data=data,
+            content_type='application/json',
+            headers={
+                'X-Slack-Request-Timestamp': timestamp,
+                'X-Slack-Signature': signature
+            }
+        )
+        assert res.status_code == 403
+
+    assert test_error_signature.event_handled
