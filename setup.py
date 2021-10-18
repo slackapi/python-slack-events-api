8fa8e6bd72ddfc0e921b716c88cf0bfd773ffa1c
Kazuhiro Sera <ksera@slack-corp.com>
2021-10-18 13:57:22 +0900

    Add Python 3.10 support

--- setup.py
+++ setup.py
@@ -49,5 +49,6 @@ setup(name='slackeventsapi',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
       ],
       zip_safe=False)
