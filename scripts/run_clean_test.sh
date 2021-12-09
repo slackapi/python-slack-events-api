2884d7d21fea634d1e5e7926409ed87f6fcc14cf
Kazuhiro Sera <ksera@slack-corp.com>
2021-12-09 15:57:26 +0900

    verson 3.0.1

--- scripts/run_clean_test.sh
+++ scripts/run_clean_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pip install -U pip && \
 pip freeze | grep -v '-e' | xargs pip uninstall -y && \
+pip install MarkupSafe && \
 python setup.py install && \
 pip install -r requirements-dev.txt && \
 pytest tests
